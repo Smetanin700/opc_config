75c02e448839ffb1af58130037592d268f5ea3f9
Digischh <67974952+ColosusPX@users.noreply.github.com>
2024-03-07 12:28:52 +0400

    add json lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <open62541/server.h>
 #include <open62541/server_config_default.h>
+#include <fstream>
 
 #include <signal.h>
 #include <stdlib.h>
+
+#include "../include/json.hpp"
+
+using json = nlohmann::json;
 
 static volatile UA_Boolean running = true;
 static void stopHandler(int sig) {
@@ -10,14 +15,19 @@ static void stopHandler(int sig) {
 }
 
 int main() {
-    signal(SIGINT, stopHandler);
-    signal(SIGTERM, stopHandler);
+    std::ifstream f("../server1.json");
+    json data = json::parse(f);
 
-    UA_Server *server = UA_Server_new();  
-    UA_ServerConfig_setDefault(UA_Server_getConfig(server));
+    char * s = data["Node"][0]["type"];
 
-    UA_StatusCode ret = UA_Server_run(server, &running);
+    // signal(SIGINT, stopHandler);
+    // signal(SIGTERM, stopHandler);
 
-    UA_Server_delete(server);
-    return ret == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
+    // UA_Server *server = UA_Server_new();  
+    // UA_ServerConfig_setDefault(UA_Server_getConfig(server));
+
+    // UA_StatusCode ret = UA_Server_run(server, &running);
+
+    // UA_Server_delete(server);
+    // return ret == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
 }
